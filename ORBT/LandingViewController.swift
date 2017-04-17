1fb0e33bae92d464264cecde31b302dbce6b90f0
Admin User <admin@Hamids-iMac-2.local>
2017-04-17 11:56:34 +0400

    start

---
+++ ORBT/LandingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LandingViewController.swift
+//  ORBT
+//
+//  Created by Admin User on 4/17/17.
+//  Copyright © 2017 Admin User. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignUpButton(_ sender: Any) {
+    }
+    
+}
+
